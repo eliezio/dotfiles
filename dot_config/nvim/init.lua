4e3b0e21569a0be5271de1a1009ecbfdc4cfc830
Eliezio Oliveira <eliezio@pm.me>
2026-05-29 12:38:00 +0100

    neovim: enable Bash/Python/Typescript LSPs

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -686,16 +686,17 @@ do
   --  See `:help lsp-config` for information about keys and how to configure
   ---@type table<string, vim.lsp.Config>
   local servers = {
+    bashls = {},
     -- clangd = {},
     -- gopls = {},
-    -- pyright = {},
+    pyright = {},
     -- rust_analyzer = {},
     --
     -- Some languages (like typescript) have entire language plugins that can be useful:
     --    https://github.com/pmizio/typescript-tools.nvim
     --
     -- But for many setups, the LSP (`ts_ls`) will work just fine
-    -- ts_ls = {},
+    ts_ls = {},
 
     stylua = {}, -- Used to format Lua code
 
@@ -759,7 +760,9 @@ do
   require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
   for name, server in pairs(servers) do
-    vim.lsp.config(name, server)
+    if server then
+      vim.lsp.config(name, server)
+    end
     vim.lsp.enable(name)
   end
 end
